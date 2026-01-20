b547ed752d2156bf00aa55fd38bfab63c563a2e9
Ami <ami@ccrsxx.com>
2026-01-20 07:46:51 +0700

    feat(main): implement graceful server shutdown

--- src/cmd/api/main.go
+++ src/cmd/api/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"context"
 	"log/slog"
-	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/ccrsxx/api-go/src/internal/config"
 	"github.com/ccrsxx/api-go/src/internal/server"
@@ -11,10 +14,36 @@ import (
 func main() {
 	server := server.NewServer()
 
-	slog.Info("server started", "port", server.Addr, "env", config.Env().AppEnv)
+	go func() {
+		slog.Info("server start listening", "port", server.Addr, "env", config.Env().AppEnv)
 
-	if err := server.ListenAndServe(); err != nil {
-		slog.Error("server failed", "error", err)
-		os.Exit(1)
+		if err := server.ListenAndServe(); err != nil {
+			slog.Error("server stop listening", "error", err)
+		}
+	}()
+
+	shutdown, stop := signal.NotifyContext(context.Background(), syscall.SIGTERM, syscall.SIGINT)
+
+	defer stop()
+
+	<-shutdown.Done()
+
+	slog.Info("server stopping gracefully")
+
+	// Allow forced stop signal to exit immediately
+	// Use case: if graceful shutdown is waiting too long, user can send
+	// a second signal (CTRL+C) to force stop the application immediately
+	stop()
+
+	// Give the server 60 seconds to shutdown gracefully
+	// Basically a hard timeout to avoid hanging forever
+	shutdownCtx, cancel := context.WithTimeout(context.Background(), 60*time.Second)
+
+	defer cancel()
+
+	if err := server.Shutdown(shutdownCtx); err != nil {
+		slog.Error("server shutdown failed", "error", err)
 	}
+
+	slog.Info("server stopped gracefully")
 }
