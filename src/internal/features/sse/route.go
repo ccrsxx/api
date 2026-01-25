187020eb92b38b9653b8c3f0d2c9d4ffcda21687
Ami <ami@ccrsxx.com>
2026-01-25 20:17:54 +0700

    feat: add rate limit on sse endpoint

--- src/internal/features/sse/route.go
+++ src/internal/features/sse/route.go
@@ -2,15 +2,19 @@ package sse
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/ccrsxx/api-go/src/internal/features/auth"
+	middlewarePackage "github.com/ccrsxx/api-go/src/internal/middleware"
 )
 
 func LoadRoutes(router *http.ServeMux) {
 	router.Handle("GET /sse",
 		auth.Middleware.IsAuthorizedFromQuery(
-			Middleware.IsConnectionAllowed(
-				http.HandlerFunc(Controller.getCurrentPlayingSSE),
+			middlewarePackage.RateLimit(10, 10*time.Second)(
+				Middleware.IsConnectionAllowed(
+					http.HandlerFunc(Controller.getCurrentPlayingSSE),
+				),
 			),
 		),
 	)
