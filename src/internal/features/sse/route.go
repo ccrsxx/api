dd683f9e3b9a191d9b5916b5df9985506dea2659
Ami <ami@ccrsxx.com>
2026-02-18 00:02:50 +0700

    refactor(middleware): simplify middleware import for clarity

--- src/internal/features/sse/route.go
+++ src/internal/features/sse/route.go
@@ -5,13 +5,13 @@ import (
 	"time"
 
 	"github.com/ccrsxx/api/src/internal/features/auth"
-	middlewarepackage "github.com/ccrsxx/api/src/internal/middleware"
+	m "github.com/ccrsxx/api/src/internal/middleware"
 )
 
 func LoadRoutes(router *http.ServeMux) {
 	router.Handle("GET /sse",
 		auth.Middleware.IsAuthorizedFromQuery(
-			middlewarepackage.RateLimit(10, 10*time.Second)(
+			m.RateLimit(10, 10*time.Second)(
 				Middleware.IsConnectionAllowed(
 					http.HandlerFunc(Controller.getCurrentPlayingSSE),
 				),
