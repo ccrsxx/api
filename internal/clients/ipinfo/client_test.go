f298f9422d5fc30e9a893ff285a5d9b70aadfdc0
Ami <ami@ccrsxx.com>
2026-04-10 15:54:18 +0700

    refactor(ipinfo): remove DefaultClient and simplify client initialization

--- internal/clients/ipinfo/client_test.go
+++ internal/clients/ipinfo/client_test.go
@@ -4,8 +4,8 @@ import (
 	"testing"
 )
 
-func TestDefaultClient(t *testing.T) {
-	client := DefaultClient()
+func TestNew(t *testing.T) {
+	client := NewClient("test-token")
 
 	if client == nil {
 		t.Fatal("got nil, want ipinfo client to be initialized")
