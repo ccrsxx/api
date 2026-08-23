e1d2de4be2d3838189118358c5887a1d24ae2a43
Ami <ami@ccrsxx.com>
2026-08-23 21:13:16 +0700

    style(model): format Content struct fields

--- internal/model/contents.go
+++ internal/model/contents.go
@@ -3,8 +3,8 @@ package model
 import "time"
 
 type Content struct {
-	Slug      string     `json:"slug"`
-	Type      string     `json:"type"`
+	Slug      string    `json:"slug"`
+	Type      string    `json:"type"`
 	Views     int64     `json:"views"`
 	Likes     int64     `json:"likes"`
 	CreatedAt time.Time `json:"createdAt,omitzero"`
